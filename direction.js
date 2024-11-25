79ea19aa77ebc2fef47f15de98ed78a7936abb06
M3hTi <mehdinasir502@yahoo.com>
2024-11-25 15:24:00 +0330

    add current position of client device with geolocation API

--- direction.js
+++ direction.js
@@ -1,6 +1,6 @@
 function initMap() {
     const map  = L.map('map', {
-        center:[39.95, -83.12],
+        center:[50, 8],
         zoom: 15
     })
 
@@ -10,7 +10,37 @@ function initMap() {
 
 
     
-    L.marker([39.95, -83.12]).addTo(map).bindPopup('Oak Top House').openPopup();
+    L.marker([50, 8]).addTo(map).bindPopup('Oak Top House').openPopup();
+
+    // console.log(navigator);
+
+    navigator.geolocation.getCurrentPosition(getPos, handleError)
+
+    function getPos(pos) {
+        // console.log(pos);
+        const myPos = {}
+        myPos["lat"] = pos.coords.latitude;
+        myPos["lng"] = pos.coords.longitude;
+        console.log(myPos);
+        L.marker(myPos).addTo(map).bindPopup('Your Location').openPopup();
+    }
+
+    function handleError(err) {
+        // console.log(err);
+        switch (err.code) {
+            case err.PERMISSION_DENIED:
+                alert("Error: Access denied by user");
+                break;
+            case err.POSITION_UNAVAILABLE:
+                alert("Error: Position unavailable");
+                break;
+            case err.TIMEOUT:
+                alert("Error: Timeout");
+                break;
+            default:
+                alert(`Error: ${err.message}`);
+        }
+    }
 }
 
 
